c50ffea0e410986d10d8d6cc51a58638ae14be32
Akhmad Zaki <akhmadzaki2@gmail.com>
2026-09-18 15:05:31 +0700

    Delete unused UI method

--- src/ui/common_ui.c
+++ src/ui/common_ui.c
@@ -29,19 +29,3 @@ void ui_error_blind_signing(void) {
                        ui_menu_main);
 #endif
 }
-
-void ui_warning_blind_signing(void) {
-#ifdef SCREEN_SIZE_WALLET
-    nbgl_useCaseChoice(&ICON_APP_WARNING,
-                       "This transaction cannot be clear-signed",
-                       "Enable blind signing in the settings to sign this transaction.",
-                       "Go to settings",
-                       "Reject transaction",
-                       ui_error_blind_signing_choice);
-#else
-    nbgl_useCaseAction(&C_Alert_circle_14px,
-                       "Blind signing must\nbe enabled in\nsettings",
-                       NULL,
-                       ui_menu_main);
-#endif
-}
